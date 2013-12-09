43856776c8add9684f7cd4912bdd346aaa5a3b6b
bluelabeldeveloper1 <bluelabeldeveloper1@90c39dbc-b7aa-4732-b7bf-8bdacfab3e7a>
2013-12-09 08:43:00 +0000

    add new parameter in issue point1
    git-svn-id: https://bluelabellabs.svn.cloudforge.com/bl049@26 90c39dbc-b7aa-4732-b7bf-8bdacfab3e7a

--- Brewhorn-Server/trunk/application/models/webservicemodel.php
+++ Brewhorn-Server/trunk/application/models/webservicemodel.php
@@ -710,9 +710,11 @@ class webservicemodel extends CI_Model {
                                         //add facebook twitter parameter
                                         $FacebookData = $this->db->select()->where('beerId',$beerId)->where('networkId','1')->from('socialMediaAcct')->get()->row_array();
                                         if($FacebookData):
-                                            $facebook = $FacebookData['url'];
+                                            $facebook = isset($FacebookData['handle'])?$FacebookData['handle']:'';
+                                            $facebookurl = isset($FacebookData['url'])?$FacebookData['url']:'';
                                         else:
                                             $facebook = '';
+                                            $facebookurl = '';
                                         endif;
                                         
                                         $twitterData = $this->db->select()->where('beerId',$beerId)->where('networkId','2')->from('socialMediaAcct')->get()->row_array();
@@ -768,9 +770,9 @@ class webservicemodel extends CI_Model {
 					
 					
 					
-					$bearArr[] = array("beerId"=>$beerId,"aroma"=>$result['aroma'],"sweet"=>$result['sweet'],"bitter"=>$result['bitter'],"malt"=>$result['malt'],"yeast"=>$result['yeast'],"mouthFeel"=>$result['mouthFeel'],"sour"=>$result['sour'],"additive"=>$result['additive'],"booziness"=>$result['booziness'],"brewery"=>$brewery,"beerName"=>$beerName,"beerStyle"=>$beerStyle,"abv"=>$abv,"ibu"=>$ibu,"mood"=>$mood,"venue"=>$venue,"event"=>$event,"hype"=>$hype,"tastePercentage"=>$tastePercentage,"beerBottle"=>$beerBottle,"facebook"=>$facebook,"twitter"=>$twitter);
+					$bearArr[] = array("beerId"=>$beerId,"aroma"=>$result['aroma'],"sweet"=>$result['sweet'],"bitter"=>$result['bitter'],"malt"=>$result['malt'],"yeast"=>$result['yeast'],"mouthFeel"=>$result['mouthFeel'],"sour"=>$result['sour'],"additive"=>$result['additive'],"booziness"=>$result['booziness'],"brewery"=>$brewery,"beerName"=>$beerName,"beerStyle"=>$beerStyle,"abv"=>$abv,"ibu"=>$ibu,"mood"=>$mood,"venue"=>$venue,"event"=>$event,"hype"=>$hype,"tastePercentage"=>$tastePercentage,"beerBottle"=>$beerBottle,"facebook"=>$facebook,"facebookurl "=>$facebookurl,"twitter"=>$twitter);
 				else:
-					$bearArr[] = array("beerId"=>$beerId,"aroma"=>"0","sweet"=>"0","bitter"=>"0","malt"=>"0","yeast"=>"0","mouthFeel"=>"0","sour"=>"0","additive"=>"0","booziness"=>"0","brewery"=>$brewery,"beerName"=>$beerName,"beerStyle"=>$beerStyle,"abv"=>$abv,"ibu"=>$ibu,"mood"=>$mood,"venue"=>$venue,"event"=>$event,"hype"=>$hype,"tastePercentage"=>0,"beerBottle"=>0,"facebook"=>"","twitter"=>"");
+					$bearArr[] = array("beerId"=>$beerId,"aroma"=>"0","sweet"=>"0","bitter"=>"0","malt"=>"0","yeast"=>"0","mouthFeel"=>"0","sour"=>"0","additive"=>"0","booziness"=>"0","brewery"=>$brewery,"beerName"=>$beerName,"beerStyle"=>$beerStyle,"abv"=>$abv,"ibu"=>$ibu,"mood"=>$mood,"venue"=>$venue,"event"=>$event,"hype"=>$hype,"tastePercentage"=>0,"beerBottle"=>0,"facebook"=>"","facebookurl "=>"","twitter"=>"");
 				endif; 
 			endforeach;
 			//die;
